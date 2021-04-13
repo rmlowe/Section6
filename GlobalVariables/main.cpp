a15c73b5ef23459d6b81a5384f0861f3a12fd2c3
Robert Lowe <robertmlowe@rmlowe.com>
2021-04-13 22:58:37 +0800

    Global Variables

---
+++ GlobalVariables/main.cpp
@@ -0,0 +1,18 @@
+// Section 6
+// Global and local variables
+
+#include <iostream>
+
+using namespace std;
+
+int age {18}; // Global variable
+ 
+int main() {
+    
+    int age {16};
+    
+    cout << age << endl;
+    
+    return 0;
+}
+
